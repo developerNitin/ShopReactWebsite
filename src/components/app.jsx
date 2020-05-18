0d53147eeb2dfe747dc0c15ce8e06e273150a2ae
Nitin Birdi <nitin.birdi@icloud.com>
2020-05-18 17:54:01 +0530

    #2_commit:

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./sub-components/header.jsx";
+import Home from "./home.jsx";
+
+import Footer from "./sub-components/footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/sub-components/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../css/sub-css/header.css"
+
+function Header() {
+  return (
+    <div className="main-header">
+      <div className="sub-main-header-div">
+        <div className="header-logo">
+          <a></a>
+        </div>
+
+        <div className="header-user"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
